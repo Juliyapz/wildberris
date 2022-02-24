6ffe34bed3f95f575601173c105554e6020eba2a
Juliyapz <76488867+Juliyapz@users.noreply.github.com>
2022-02-24 15:19:49 +0900

    day 2

---
+++ js/getGoods.js
@@ -0,0 +1,19 @@
+const getGoods = () => {
+  const links = document.querySelectorAll('.navigation-link')
+
+  const getData = () => {
+    fetch('https://wildberris-894e4-default-rtdb.firebaseio.com/db.json')
+      .then((res) => res.json())
+      .then((data) => {
+        localStorage.setItem('data', JSON.stringify(data))
+      })
+  }
+
+  links.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault
+      getData()
+    })
+  })
+}
+getGoods()
